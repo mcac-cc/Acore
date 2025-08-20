b7b0f36763086592a92844d5a3025ae996acd0fa
SOTO <z@mcac.cc>
2025-08-21 00:13:55 +0800

    add mirai bot

--- src/main/java/cc/mcac/acore/Acore.java
+++ src/main/java/cc/mcac/acore/Acore.java
@@ -1,22 +1,33 @@
 package cc.mcac.acore;
 
+import cc.mcac.acore.bot.MessageListener;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
 
 @Plugin(
-    id = "acore",
-    name = "Acore",
-    version = BuildConstants.VERSION
-    ,authors = {"acsoto"}
+        id = "acore",
+        name = "Acore",
+        version = BuildConstants.VERSION
+        , authors = {"acsoto"}
 )
 public class Acore {
 
-    @Inject private Logger logger;
+    private final ProxyServer server;
+    private final Logger logger;
+
+    @Inject
+    public Acore(ProxyServer server, Logger logger) {
+        this.server = server;
+        this.logger = logger;
+        logger.info("Acore plugin initialized with version: {}", BuildConstants.VERSION);
+    }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+        server.getEventManager().register(this,new MessageListener(server));
     }
 }
